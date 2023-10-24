a8cd9052e0e6496e44ac3cab1ea86cc2ffec5426
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:55:51 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_tdee_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for TDEE Calculator by Calculator.iO";
 
 function display_ci_tdee_calculator(){
